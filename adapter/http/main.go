fce41d20f9e875b8c5b2776488d266d0847f73ab
Felipe Carvalho <P5180242@brasilprev.com.br>
2020-08-24 00:49:34 -0300

    healthcheck request

--- adapter/http/main.go
+++ adapter/http/main.go
@@ -2,11 +2,14 @@ package http
 
 import (
 	"github.com/07carvalho/dio-expert-finance/adapter/http/transaction"
+	"github.com/07carvalho/dio-expert-finance/adapter/http/actuator"
 	"net/http"
 )
 
 func Init() {
 	http.HandleFunc("/transactions", transaction.GetTransactions)
 	http.HandleFunc("/transactions/create", transaction.CreateTransaction)
+	http.HandleFunc("/health", actuator.Health)
+
 	http.ListenAndServe(":8888", nil)
 }
